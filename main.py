7ed7b896b731bb1ca7eb7c1f8c39836b61b275dd
m1981 <m.nakiewicz@gmail.com>
2023-08-13 15:59:46 +0000

    Accept and Reject and No orders works :)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import requests
 import base64
 import itertools
+from operator import itemgetter
 
 app = Flask(__name__)
 
@@ -27,20 +28,27 @@ def change_order_status(order_id, status):
     response = requests.put(order_update_url, headers=headers, json=data)
     response.raise_for_status()
 
-
-@app.route('/')
-def table():
+def get_oldest_on_hold_order():
     base64_encoded_data = base64.b64encode(f"{client_key}:{client_secret}".encode("utf-8")).decode("utf-8")
     response = requests.get(
         "https://fabrykasmakow.com.pl/wp-json/wc/v3/orders",
         headers={"Authorization": f"Basic {base64_encoded_data}"}
     )
     data = response.json()
-
-    sorted_data = sorted(data, key=lambda x: x['order_key'])
-    grouped_data = [{'order_key': k, 'orders': list(v)} for k, v in itertools.groupby(sorted_data, key=lambda x: x['order_key'])]
     
-    return render_template('table.html', data=grouped_data)
+    # Filter for 'on-hold' orders
+    on_hold_orders = [order for order in data if order['status'] == "on-hold"]
+    
+    # Sort 'on-hold' orders by date created
+    sorted_orders = sorted(on_hold_orders, key=itemgetter('date_created'))
+    
+    # Return the oldest 'on-hold' order if it exists, otherwise None
+    return sorted_orders[0] if sorted_orders else None
+
+@app.route('/')
+def table():
+    order = get_oldest_on_hold_order()
+    return render_template('table.html', order=order)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
