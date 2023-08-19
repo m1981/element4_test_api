67f1721e7d5c7e4079d3c91e8a67bd0efb7b5b0d
michal <m.nakiewicz@gmail.com>
2023-08-19 13:43:54 +0200

    dev not working

--- receipt.py
+++ receipt.py
@@ -61,19 +61,14 @@ class Printer:
         self.speed = speed
         self.timeout = timeout
 
-    def init(self):
+    def print_receipt(self, order):
+        W = ctypes.c_int()
+        OpisBledu = ctypes.create_string_buffer(255)
+
         if self.elzabdr.CommunicationInit(self.port, self.speed, self.timeout) != 0:
             raise Exception('Cannot init printer')
 
-    def end(self):
-        if self.elzabdr.CommunicationEnd() != 0:
-            raise Exception('Cannot end printer communication')
-
-    def print_receipt(self, order):
         try:
-            self.init()
-            W = ctypes.c_int()
-            OpisBledu = ctypes.create_string_buffer(255)
             self.elzabdr.pFillLines(2, order.shop_name.encode('utf-8'), ctypes.byref(W))
             if W.value != 0:
                 elzabdr.pErrMessage(W.value, OpisBledu)
@@ -90,47 +85,21 @@ class Printer:
                     return
             elzabdr.ReceiptEnd(0)
         finally:
-            self.end()
+            if self.elzabdr.CommunicationEnd() != 0:
+                raise Exception('Cannot end printer communication')
 
+    def print_internal_order(self, order, port):
+        W = ctypes.c_int()
+        OpisBledu = ctypes.create_string_buffer(255)
 
-    def print_internal_order(self, order):
-        try:
-            self.init()
-            for item in order.items:
-                self.elzabdr.NonFiscalPrintoutBegin(53)
-                self.elzabdr.pNonFiscalPrintoutLine(10, b"", 0)
-                self.elzabdr.pNonFiscalPrintoutLine(40, b"TowarTestowy_A", 1)
-                self.elzabdr.pNonFiscalPrintoutLine(40, b"TowarTestowy_B", 1)
+        if self.elzabdr.CommunicationInit(port, self.speed, self.timeout) != 0:
+            raise Exception('Cannot init printer')
 
-                self.elzabdr.pNonFiscalPrintoutLine(1, b"Numer kolejny: 34", 1)
-                self.elzabdr.pNonFiscalPrintoutLine(1, b"Wynos", 1)
-                # Max line length is 36 characters
-                self.elzabdr.pNonFiscalPrintoutLine(1, "Bez boczku i mięsa bo jeste weganin".encode('utf-8'), 1)
-                self.elzabdr.pNonFiscalPrintoutLine(1, "em. Poprosze ekstra sałatę i na godzię 12:00".encode('utf-8'), 1)
-                self.elzabdr.pNonFiscalPrintoutLine(11, b"Telefon", 1)
-
-            # Zapisz zamownienie do pliku
-            # Zwieksz numer zamowienia
-
-            # Print EAN code
-            self.elzabdr.pNonFiscalPrintoutLine(21, b"791630003", 1);
-            #
-            self.elzabdr.NonFiscalPrintoutEnd()
-            if wynik == 0:
-                print("Program zakończony bezbłędnie")
-        finally:
-            self.end()
 
 if __name__ == "__main__":
-    # Setting up the printer
     printer = Printer(elzabdr, 2, 9600, 5)
-
-    # Creating an order
     order = Order('Sklep internetowy', '1234567890')
-
-    # Adding items
     order.add_item(ReceiptItem('TowarTestowy_A', 1, 0, 100, 2, 'szt.'))
     order.add_item(ReceiptItem('TowarTestowy_B', 1, 0, 100, 2, 'szt.'))
-
-    # Printing
     printer.print_receipt(order)
+    #printer.print_internal_order(order, 2)
