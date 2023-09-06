af32c6cabcf3c130a8dcff2666fef1a12b0da360
michal <m.nakiewicz@gmail.com>
2023-09-06 10:12:16 +0200

    version.py removed from ci build

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-call "C:\\hostedtoolcache\\windows\\Python\\3.11.4\\x64\\python.exe" -m PyInstaller --onefile --name %APPNAME% orders_receiver.py receipt.py version.py
+call "C:\\hostedtoolcache\\windows\\Python\\3.11.4\\x64\\python.exe" -m PyInstaller --onefile --name %APPNAME% orders_receiver.py receipt.py
 
 REM Local build
 REM call "C:\Users\nakiewic\AppData\Local\Programs\Python\Python311\python.exe" -m PyInstaller --onefile --name %APPNAME% orders_receiver.py receipt.py
