73d1acb72ee67fceea27e3cba4c5f61680320f33
michal <m.nakiewicz@gmail.com>
2023-09-06 07:46:10 +0200

    fix for build on CI

--- build.bat
+++ build.bat
@@ -1,21 +1,5 @@
 @echo off
-REM Fetch the latest git tag
-for /f "delims=" %%i in ('git describe --tags --dirty') do @set VERSION=%%i
+call "C:\\hostedtoolcache\\windows\\Python\\3.11.4\\x64\\python.exe" -m PyInstaller --onefile --name %APPNAME% orders_receiver.py receipt.py version.py
 
-REM Replace the version in version.py
-echo __version__ = "%VERSION%" > version.py
-
-REM Fetch the current date and time
-for /f "tokens=2 delims==." %%i in ('wmic os get localdatetime /format:list') do @set datetime=%%i
-set BUILD_DATE=%datetime:~6,2%.%datetime:~4,2%.%datetime:~0,4%
-set BUILD_TIME=%datetime:~8,2%.%datetime:~10,2%
-
-REM Define application name
-set APPNAME=orders_receiver_%VERSION%__built-%BUILD_DATE%__%BUILD_TIME%
-
-REM Replace the version and build date/time in version.py
-echo __version__ = "%VERSION%" > version.py
-echo __build_date__ = "%BUILD_DATE%" >> version.py
-echo __build_time__ = "%BUILD_TIME%" >> version.py
-
-python.exe -m PyInstaller --onefile --name %APPNAME% orders_receiver.py receipt.py version.py
+REM Local build
+REM call "C:\Users\nakiewic\AppData\Local\Programs\Python\Python311\python.exe" -m PyInstaller --onefile --name %APPNAME% orders_receiver.py receipt.py version.py
