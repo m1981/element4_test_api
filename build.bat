40bced9736752daa2a94e49ef0f2d771870c6ea3
michal <m.nakiewicz@gmail.com>
2023-09-06 09:13:16 +0200

    version.py removed from build command

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 call "C:\\hostedtoolcache\\windows\\Python\\3.11.4\\x64\\python.exe" -m PyInstaller --onefile --name %APPNAME% orders_receiver.py receipt.py version.py
 
 REM Local build
-REM call "C:\Users\nakiewic\AppData\Local\Programs\Python\Python311\python.exe" -m PyInstaller --onefile --name %APPNAME% orders_receiver.py receipt.py version.py
+REM call "C:\Users\nakiewic\AppData\Local\Programs\Python\Python311\python.exe" -m PyInstaller --onefile --name %APPNAME% orders_receiver.py receipt.py
