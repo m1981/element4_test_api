2b4c5d8e3b71922c3419190172b5adac4340ea0a
michal <m.nakiewicz@gmail.com>
2023-09-10 19:51:34 +0200

    run app script added

---
+++ run _sms.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Fetch the latest git tag
+for /f "delims=" %%i in ('git describe --tags --dirty') do @set VERSION=%%i
+
+REM Replace the version and build date/time in version.py
+echo __version__ = "%VERSION%" > version.py
+echo __build_date__ = "Now" >> version.py
+echo __build_time__ = "Now" >> version.py
+
+
+"C:\Program Files\Python311\python.exe" orders_sms.py
+pause
