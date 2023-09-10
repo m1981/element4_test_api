e4cb35ea7b947a16990073757b38230cd302d036
michal <m.nakiewicz@gmail.com>
2023-09-10 22:24:14 +0200

    dev: tests added

--- tests/test_sms_composer.py
+++ tests/test_sms_composer.py
@@ -1,6 +1,29 @@
 import pytest
+from unittest.mock import MagicMock, patch, call
+from modules.sms_composer import SMSComposer
+from modules.sms_dispatcher import SMSDispatcher
 
 def test_compose_sms(mock_sms_composer):
-    mock_sms_composer.compose_sms('mock_phone_number', 'mock_message')
-    # Add relevant assertion(s) here
-    pass
+    # Setup - Define phone number and message values
+    phone_number = '123456789'
+    message = 'Test message'
+
+    # Exercise - Call the compose_sms method
+    with patch.object(mock_sms_composer, '_write_with_pause') as mock_write_with_pause:
+        mock_sms_composer.compose_sms(phone_number, message)
+
+    # Verify - Assert that the _write_with_pause method was called with correct parameters
+    calls = [call('AT+CMGF=1\r'), call(f'AT+CMGS="{phone_number}"\r'), call(f'{message}\r')]
+    mock_write_with_pause.assert_has_calls(calls)
+
+
+def test_send_sms_while_another_being_sent(mock_sms_composer):
+    dispatcher = SMSDispatcher(mock_sms_composer)
+    with patch('modules.sms_composer.SMSComposer._write_with_pause',
+               side_effect=[None, Exception("Port busy")]), patch('logging.error') as mock_logger:
+        try:
+            dispatcher.send_sms('1234567890', 'test message')
+            dispatcher.send_sms('1234567890', 'test message')
+        except Exception:
+            pass
+        assert mock_logger.called
--- tests/test_sms_dispatcher.py
+++ tests/test_sms_dispatcher.py
@@ -1,7 +1,25 @@
 import pytest
+from unittest.mock import MagicMock, patch, call
 from modules.sms_dispatcher import SMSDispatcher
 
 
 def test_send_sms(fixture_sms_dispatcher):
-    # Implement the test here
-    pass
+    # Setup - Define phone number
+    phone_number = '123456789'
+    message = 'Test message'
+
+    # Mock the compose_sms and _write_with_pause methods
+    with patch.object(fixture_sms_dispatcher._composer, 'compose_sms') as mock_compose:
+        with patch.object(fixture_sms_dispatcher._composer, '_write_with_pause') as mock_write:
+
+            # Call the send_sms method
+            fixture_sms_dispatcher.send_sms(phone_number, message)
+
+            # Assert that compose_sms was called once with correct arguments
+            mock_compose.assert_called_once_with(phone_number, message)
+
+            # Assert that _write_with_pause was called once with the correct argument
+            mock_write.assert_called_once_with(chr(26))
+
+
+
