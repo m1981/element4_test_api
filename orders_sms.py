75ffa8038fb6d4847f66eb16316c1a5b720dbb10
michal <m.nakiewicz@gmail.com>
2023-09-10 23:30:24 +0200

    dev: scan number and config added

--- orders_sms.py
+++ orders_sms.py
@@ -1,12 +1,52 @@
+import serial
+import threading
+import time
 import tkinter as tk
+from modules.serial_connection import SerialConnection
 
 class Application:
-    def __init__(self, master):
+    def __init__(self, master, config):
         self.master = master
+        self.config = config
         self.default_font = font = ("Verdana", 10)
-        
+
         self.init_master()
         self.create_buttons()
+
+        self.scanner_port = self.config['scanner_port']
+        self.sms_port = self.config['sms_port']
+        self.use_local_file_as_scanner = self.config['use_local_file_as_scanner']
+        self.use_console_as_sms = self.config['use_console_as_sms']
+        self.connection = SerialConnection()
+        self.scanner_thread = threading.Thread(target=self.scan_number, daemon=True)
+        self.scanner_thread.start()
+
+    def scan_number(self):
+        while True:
+            try:
+                ser = self.connection.create_serial_connection(self.scanner_port)
+
+                if ser:
+                    barcode_data = ser.readline().decode('utf-8').strip()
+                    time.sleep(0.2)  # delay for 200 ms
+                    if len(barcode_data) > 0:
+                        self.update_label(barcode_data)  # update the label with read phone number
+
+            except serial.SerialException as e:
+                # handle the error gracefully
+                # logging.error(f'Error occurred while connecting to {self.scanner_port}: {str(e)}')
+                print(f"Cannot open port {self.scanner_port}. Ensure it's valid and not in use.")
+                self.update_label("Scanning...")  # revert to "Scanning..." status in case of exception.
+            finally:
+                if ser.isOpen():
+                    ser.close()
+
+
+    def update_label(self, phone_number=None):
+        if phone_number:
+            self.label_actions.config(text=str(phone_number))
+        else:
+            self.label_actions.config(text="Scanning...")
 
     def init_master(self):
         self.master.geometry("300x50")
@@ -20,7 +60,7 @@ class Application:
         offset_right = 20
         app_height = 80
         app_width = 400
-        position_right = screen_width - app_width - offset_right 
+        position_right = screen_width - app_width - offset_right
         self.master.geometry("{}x{}+{}+{}".format(app_width, app_height, position_right, position_top))
 
     def create_buttons(self):
@@ -28,8 +68,8 @@ class Application:
         frame_buttons.pack(pady=2)
 
         # added label above the buttons
-        label_actions = tk.Label(frame_buttons, text="Order Actions", font=self.default_font)
-        label_actions.pack(pady=5)
+        self.label_actions = tk.Label(frame_buttons, text="Order Actions", font=self.default_font)
+        self.label_actions.pack(pady=5)
 
         button_przyjmij = tk.Button(frame_buttons, text="Przyjmij", font=self.default_font, width=8, height=1, bg='#00b4c9', fg='#FFFFFF')
         button_przyjmij.pack(side='left', padx=20)
@@ -38,6 +78,17 @@ class Application:
         button_wydaj.pack(side='left', padx=2)
 
 
-root = tk.Tk()
-app = Application(root)
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+
+    with open("config_sms.yaml", 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+            app = Application(root, config)
+        except FileNotFoundError:
+            print("The configuration file was not found.")
+        except yaml.YAMLError as exc:
+            print(f"Error in configuration file: {exc}")
+
+    root.mainloop()
+
