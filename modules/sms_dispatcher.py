3108e7a62063778433f52c3902aed867c9f884d8
michal <m.nakiewicz@gmail.com>
2023-09-11 19:38:11 +0200

    reset modem command added

--- modules/sms_dispatcher.py
+++ modules/sms_dispatcher.py
@@ -4,8 +4,12 @@ class SMSDispatcher:
     def __init__(self, composer):
         self._composer = composer
 
+    def reset_modem(self):
+        self._composer._write_with_pause('ATZ\r')
+
     def send_sms(self, phone_number, message):
         try:
+            self.reset_modem()
             self._composer.compose_sms(phone_number, message)
             self._composer._write_with_pause(chr(26))
             logging.info("Message sent successfully!")
