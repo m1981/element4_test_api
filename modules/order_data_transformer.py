f429ce1f37ffdafeea20ae4e6ce5cd2b11a6e5de
Michal <m.nakiewicz@gmail.com>
2023-09-25 21:43:32 +0200

    dev: OrderDataTransformer added with tests

--- modules/order_data_transformer.py
+++ modules/order_data_transformer.py
@@ -4,25 +4,32 @@ from modules.dtos.line_item_dto import LineItemDto
 class OrderDataTransformer:
     @staticmethod
     def extract_metadata(meta_data_list):
-        is_vat_exempt = False
-        na_miejscu_na_wynos = None
-
-        for meta_data in meta_data_list:
-            if meta_data['key'] == 'is_vat_exempt':
-                is_vat_exempt = meta_data['value'] == 'yes'
-            elif meta_data['key'].startswith('na_miejscu_na_wynos'):
-                na_miejscu_na_wynos = meta_data['value']
-
+        is_vat_exempt = next((m['value'] == 'yes' for m in meta_data_list if m['key'] == 'is_vat_exempt'), False)
+        na_miejscu_na_wynos = {}
+        for m in meta_data_list:
+            if m['key'].startswith('na_miejscu_na_wynos'):
+                key_parts = m['key'].split('_')
+                index = int(key_parts[-1]) if key_parts[-1].isdigit() else 1
+                na_miejscu_na_wynos[index] = m['value']
         return is_vat_exempt, na_miejscu_na_wynos
+
 
     @staticmethod
     def transform_to_order_dto(order):
+        is_vat_exempt, na_miejscu_na_wynos = OrderDataTransformer.extract_metadata(order['meta_data'])
         line_items_dto = []
-        for item in order['line_items']:
-            is_vat_exempt, na_miejscu_na_wynos = OrderDataTransformer.extract_metadata(item['meta_data'])
-            line_item_dto = LineItemDto(item['name'], item['quantity'], item['total'], item['total_tax'], is_vat_exempt, na_miejscu_na_wynos)
+        for index, item in enumerate(order['line_items'], start=1):
+            line_item_dto = LineItemDto(
+                item['name'],
+                item['quantity'],
+                float(item['total']),
+                float(item['total_tax']),
+                is_vat_exempt,
+                na_miejscu_na_wynos.get(index)
+            )
             line_items_dto.append(line_item_dto)
-
         order_dto = OrderDto(order['id'], order['status'], order['billing'], line_items_dto, order['date_created'], order['dodatki_do_pizzy'])
         return order_dto
+
+
 
--- tests/test_order_data_transformer.py
+++ tests/test_order_data_transformer.py
@@ -1,47 +1,105 @@
 from modules.order_data_transformer import OrderDataTransformer
+from modules.dtos.order_dto import OrderDto
+from modules.dtos.line_item_dto import LineItemDto
 
 class TestOrderDataTransformer:
 
     def test_extract_metadata(self):
-        meta_data_list = [
+        transformer = OrderDataTransformer()
+        input_data = [
             {"key": "is_vat_exempt", "value": "yes"},
-            {"key": "na_miejscu_na_wynos", "value": "Na miejscu"}
+            {"key": "na_miejscu_na_wynos", "value": "Na miejscu"},
+            {"key": "na_miejscu_na_wynos_2", "value": "Na wynos"},
         ]
-        is_vat_exempt, na_miejscu_na_wynos = OrderDataTransformer.extract_metadata(meta_data_list)
-        assert is_vat_exempt == True
-        assert na_miejscu_na_wynos == "Na miejscu"
+        got_is_vat_exempt, got_na_miejscu_na_wynos = transformer.extract_metadata(input_data)
+
+        assert got_is_vat_exempt == True
+        assert got_na_miejscu_na_wynos == {1: 'Na miejscu', 2: 'Na wynos'}
+
 
     def test_transform_to_order_dto(self):
-        order = {
-            'id': 1,
-            'status': 'processing',
-            'billing': { 'payment_method': 'cash', 'payment_method_title': 'Cash on delivery', 'transaction_id': '123' },
-            'line_items': [
+        order_data = {
+            "id": 2854,
+            "status": "completed",
+            "billing": {
+                "first_name": "Test",
+                "last_name": "User",
+                "address_1": "123 Test St",
+                "address_2": "",
+                "city": "Testville",
+                "postcode": "12345",
+                "country": "Testland",
+                "phone": "1234567890"
+            },
+            "line_items": [
                 {
-                    'name': 'Item1',
-                    'quantity': 1,
-                    'total': 15.0,
-                    'total_tax': 1.5,
-                    'meta_data': [
-                        {"key": "is_vat_exempt", "value": "yes"},
-                        {"key": "na_miejscu_na_wynos", "value": "Na miejscu"}
+                    "name": "Test Product 1",
+                    "quantity": 1,
+                    "total": "10.00",
+                    "total_tax": "1.00",
+                    "meta_data": [
+                        {
+                            "key": "is_vat_exempt",
+                            "value": "no"
+                        }
+                    ]
+                },
+                {
+                    "name": "Test Product 2",
+                    "quantity": 2,
+                    "total": "20.00",
+                    "total_tax": "2.00",
+                    "meta_data": [
+                        {
+                            "key": "is_vat_exempt",
+                            "value": "no"
+                        }
                     ]
                 }
             ],
-            'date_created': '2022-10-01T00:00:00',
-            'dodatki_do_pizzy': {'notatki': 'extra cheese'}
+            "meta_data": [
+                {
+                    "id": 37759,
+                    "key": "is_vat_exempt",
+                    "value": "no"
+                },
+                {
+                    "id": 37763,
+                    "key": "na_miejscu_na_wynos",
+                    "value": "Na miejscu"
+                },
+                {
+                    "id": 37764,
+                    "key": "na_miejscu_na_wynos_2",
+                    "value": "Na wynos"
+                },
+            ],
+            "date_created": "2021-08-01T00:00:00",
+            "dodatki_do_pizzy": {
+                "dodatki_do_pizzy_popup": "",
+                "dodatki_do_sniadan": "",
+                "info_zamowienia": "",
+                "notatki": ""
+            }
         }
-        
-        order_dto = OrderDataTransformer.transform_to_order_dto(order)
-        assert order_dto.order_id == 1
-        assert order_dto.status == 'processing'
-        assert order_dto.billing == order['billing']
-        assert len(order_dto.line_items) == 1
-        assert order_dto.line_items[0].item_name == 'Item1'
-        assert order_dto.line_items[0].quantity == 1
-        assert order_dto.line_items[0].total == 15.0
-        assert order_dto.line_items[0].total_tax == 1.5
-        assert order_dto.line_items[0].is_vat_exempt == True
-        assert order_dto.line_items[0].na_miejscu_na_wynos == "Na miejscu"
-        assert order_dto.date_created == '2022-10-01T00:00:00'
-        assert order_dto.dodatki_do_pizzy == order['dodatki_do_pizzy']
+
+        order_dto = OrderDataTransformer.transform_to_order_dto(order_data)
+
+        # Assertions to ensure that the transformation happened correctly
+        assert isinstance(order_dto, OrderDto)
+        assert order_dto.order_id == order_data['id']
+        assert order_dto.status == order_data['status']
+        assert order_dto.billing == order_data['billing']
+
+        # Assert the transformation inside line items
+        assert len(order_dto.line_items) == len(order_data['line_items'])
+
+        for index, line_item in enumerate(order_dto.line_items):
+            assert isinstance(line_item, LineItemDto)
+            assert line_item.item_name == order_data['line_items'][index]['name']
+            assert line_item.quantity == order_data['line_items'][index]['quantity']
+            assert line_item.total == float(order_data['line_items'][index]['total'])
+            assert line_item.total_tax == float(order_data['line_items'][index]['total_tax'])
+            assert line_item.is_vat_exempt == (order_data['meta_data'][0]['value'] == "yes")
+            assert line_item.na_miejscu_na_wynos == order_data['meta_data'][index + 1]['value']
+
