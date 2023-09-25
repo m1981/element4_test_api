24fe239c257e3d366c0919b8dd4a396df355eb1b
Michal <m.nakiewicz@gmail.com>
2023-09-25 21:07:12 +0200

    con test added PATH

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
+import os
 import pytest
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+
 from unittest.mock import MagicMock
 from modules.sms_composer import SMSComposer
 from modules.sms_dispatcher import SMSDispatcher
