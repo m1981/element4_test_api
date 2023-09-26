aa26b0faecfb98a3040b324ad6674716a2c5b495
Michal <m.nakiewicz@gmail.com>
2023-09-26 11:16:13 +0200

    dev: test formatting for multi item order

--- tests/test_order_data_transformer_json.py
+++ tests/test_order_data_transformer_json.py
@@ -8,29 +8,26 @@ class TestOrderDataTransformerJson(unittest.TestCase):
         with open('tests/test_data/test_order_1.json') as f:
             self.order_1 = json.load(f)
 
-        # Load the test data from test_order_2.json
-        # with open('test_data/test_order_2.json') as f:
-        #     self.order_2 = json.load(f)
+        # Load the test data from test_order_2715.json
+        with open('tests/test_data/test_order_2715.json') as f:
+            self.order_2715 = json.load(f)
 
     def test_extract_metadata(self):
         # Apply the extraction method to the test data and assert the expected results
-        is_vat_exempt_1, na_miejscu_na_wynos_1 = OrderDataTransformer.extract_metadata(self.order_1['meta_data'])
-        self.assertEqual(is_vat_exempt_1, False)
+        na_miejscu_na_wynos_1 = OrderDataTransformer.extract_metadata(self.order_1['meta_data'])
         self.assertEqual(na_miejscu_na_wynos_1, {1: 'Na miejscu', 2: 'Na wynos'})
-        
-        # Repeat for the second test file
-        # is_vat_exempt_2, na_miejscu_na_wynos_2 = OrderDataTransformer.extract_metadata(self.order_2['meta_data'])
-        # self.assertEqual(is_vat_exempt_2, False)
-        # self.assertEqual(na_miejscu_na_wynos_2, {1: 'Na miejscu', 2: 'Na wynos'})
+
+    def test_extract_metadata_2715(self):
+        na_miejscu_na_wynos_2 = OrderDataTransformer.extract_metadata(self.order_2715['meta_data'])
+        self.assertEqual(na_miejscu_na_wynos_2,
+        {1: 'Na wynos', 2: 'Na wynos', 3: 'Na wynos', 4: 'Na wynos', 5: 'Na miejscu'})
     #
-    def test_transform_to_order_dto(self):
+    def test_transform_to_order_dto_2715(self):
         # Apply the transformation method to the test data and assert the expected results.
-        order_dto_1 = OrderDataTransformer.transform_to_order_dto(self.order_1)
-        self.assertEqual(order_dto_1.status, 'processing')
-    #
-    #     # Repeat for the second test file
-    #     order_dto_2 = OrderDataTransformer.transform_to_order_dto(self.order_2)
-    #     self.assertEqual(order_dto_2.status, 'processing')
+        order_dto = OrderDataTransformer.transform_to_order_dto(self.order_2715)
+        self.assertEqual(order_dto.line_items[0].na_miejscu_na_wynos, 'Na wynos')
+        self.assertEqual(order_dto.line_items[1].na_miejscu_na_wynos, 'Na wynos')
+        self.assertEqual(order_dto.line_items[2].na_miejscu_na_wynos, 'Na miejscu')
 
 if __name__ == '__main__':
     unittest.main()
